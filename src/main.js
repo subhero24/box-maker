a03c86ff291371354a96b31bbb0911508db482b7
SubHero <github@bvdsoftware.be>
2019-10-02 19:49:57 +0200

    Added typescript testing

--- src/main.js
+++ src/main.js
@@ -6,10 +6,3 @@ import ReactDOM from "react-dom"
 import Application from "./components/application"
 
 ReactDOM.render(<Application />, document.getElementById("root"))
-
-const multiply = (a, b = 2) => {
-	return a * b
-}
-
-multiply(5, 5)
-multiply("test", "woot")
