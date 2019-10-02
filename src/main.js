6137056c3e67eabcf1bf43c17c3a624e253d9974
SubHero <github@bvdsoftware.be>
2019-10-02 21:02:23 +0200

    Webpack cleanup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-// require 'file?name=main.rb!src/plugin/main.rb'
-// require 'file?name=../box-maker.rb!src/plugin/plugin.rb'
-
 import React from "react"
 import ReactDOM from "react-dom"
 import Application from "./components/application"
